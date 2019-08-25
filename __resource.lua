84245c221c9f91413e734297c9708a4cb9091593
Yücel YAVUZ <bilgi@sonsuzkaynak.com>
2019-08-25 03:06:45 +0300

    ALL

---
+++ __resource.lua
@@ -0,0 +1,15 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+Discription "Watch"
+
+Version "0.1"
+
+files {
+    "html/watch.png",
+    "html/bootstrap.min.css",
+    "html/ui.html"
+}
+ui_page "html/ui.html"
+client_script{
+    "client.lua"
+}
